9e1d5bbb99d1ef3553af1d876c35eadf125ddce5
complikatyed <katye.russell@gmail.com>
2016-10-08 14:02:57 -0500

    Update packages and set up DAL

---
+++ ZoolandiaRazor/DAL/ZoolandiaRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ZoolandiaRazor.Models;
+
+namespace ZoolandiaRazor.DAL
+{
+    public class ZoolandiaRepository
+    {
+        public ZoolandiaContext Context { get; set; }
+
+        public ZoolandiaRepository()
+        {
+            Context = new ZoolandiaContext();
+        }
+
+        public ZoolandiaRepository(ZoolandiaContext _context)
+        {
+            Context = _context;
+        }
+    }
+}
