c597fb9035d5636a3b76808d3340a0d0258fe91d
complikatyed <katye.russell@gmail.com>
2016-10-10 18:55:39 -0500

    Add initial mocking structure for 'animal' mocks

---
+++ ZoolandiaRazor.Tests/DAL/ZoolandiaRepositoryTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ZoolandiaRazor.DAL;
+using System.Collections.Generic;
+using ZoolandiaRazor.Models;
+using Moq;
+using System.Data.Entity;
+using System.Linq;
+
+
+namespace ZoolandiaRazor.Tests.DAL
+{
+    [TestClass]
+    public class ZoolandiaRepositoryTests
+    {
+        Mock<ZooContext> mock_context { get; set; }
+        Mock<DbSet<Animal>> mock_animal_table { get; set; }
+        List<Animal> animal_list { get; set; }
+        ZooRepository repo { get; set; }
+
+
+        public void ConnectMocksToDatastore()
+        {
+            var queryable_list = animal_list.AsQueryable();
+
+            
+            mock_animal_table.As<IQueryable<Animal>>().Setup(m => m.Provider).Returns(queryable_list.Provider);
+            mock_animal_table.As<IQueryable<Animal>>().Setup(m => m.Expression).Returns(queryable_list.Expression);
+            mock_animal_table.As<IQueryable<Animal>>().Setup(m => m.ElementType).Returns(queryable_list.ElementType);
+            mock_animal_table.As<IQueryable<Animal>>().Setup(m => m.GetEnumerator()).Returns(() => queryable_list.GetEnumerator());
+
+            // Animal property returns Queryable List (Fake database table).
+            mock_context.Setup(c => c.Animals).Returns(mock_animal_table.Object);
+
+            mock_animal_table.Setup(t => t.Add(It.IsAny<Animal>())).Callback((Animal a) => animal_list.Add(a));
+            mock_animal_table.Setup(t => t.Remove(It.IsAny<Animal>())).Callback((Animal a) => animal_list.Remove(a));
+        }
+
+
+    }
+
+}
